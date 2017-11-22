58a1e5cbf6002b04db9f1a2daa13442d0df60493
Jonathan Peppers <jonathan.peppers@microsoft.com>
2017-11-22 09:22:28 -0600

    Default XF template, updated NuGets
    API 26, Forms to 2.5.0, Support libs at 26.x

---
+++ xfperf/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace xfperf
+{
+	public partial class App : Application
+	{
+		public static bool UseMockDataStore = true;
+		public static string BackendUrl = "https://localhost:5000";
+
+		public App()
+		{
+			InitializeComponent();
+
+			DependencyService.Register<MockDataStore>();
+
+			if (Device.RuntimePlatform == Device.iOS)
+				MainPage = new MainPage();
+			else
+				MainPage = new NavigationPage(new MainPage());
+		}
+	}
+}
